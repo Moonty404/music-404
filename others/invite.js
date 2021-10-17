132daf80397eae23defe3cb1f216d8f8333934d6
Monty404 <83779017+Moonty404@users.noreply.github.com>
2021-10-16 17:26:30 -0700

    Update invite.js

--- others/invite.js
+++ others/invite.js
@@ -15,7 +15,11 @@ module.exports = {
     //react with approve emoji
     message.react("");
     //send the invite embed
-    message.reply(new MessageEmbed().setColor("#146DF6").setTitle("").setDescription(" [Click Here](https://discord.com/api/oauth2/authorize?client_id=794288634659995659&permissions=3439696&scope=bot) to invite the bot.")
+    message.reply(new MessageEmbed().setColor("#146DF6").setTitle("").setDescription(" [Click Here](https://discord.com/api/oauth2/authorize?client_id=
+                                                                                     
+                                                                                     
+                                                                                     
+                                                                                     &permissions=3439696&scope=bot) to invite the bot.")
     .setFooter(""));
   }
 }
