d425863d5d9ff522c662558c525aad336429c265
Monty404 <83779017+Moonty404@users.noreply.github.com>
2021-10-16 17:27:28 -0700

    Update invite.js

--- others/invite.js
+++ others/invite.js
@@ -1,25 +1,28 @@
 const { Client, Collection, MessageEmbed } = require(`discord.js`);
-const {
-  PREFIX,
-  approveemoji,
-  denyemoji
+const { 
+  PREFIX, 
 } = require(`../config.json`);
 
+  
+
+
 module.exports = {
-  name: `invite`,
-  description: `Gives you an invite`,
-  aliases: ["add"],
-  cooldown: 3,
-  edesc: "Type this command to get an invite link for the Bot, thanks for every Invite",
-  execute(message, args, client) {
-    //react with approve emoji
-    message.react("");
-    //send the invite embed
-    message.reply(new MessageEmbed().setColor("#146DF6").setTitle("").setDescription(" [Click Here](https://discord.com/api/oauth2/authorize?client_id=
-                                                                                     
-                                                                                     
-                                                                                     
-                                                                                     &permissions=3439696&scope=bot) to invite the bot.")
-    .setFooter(""));
-  }
-}
+  name: "invite",
+  aliases: ["i"],
+  cooldown: 8,
+  description: "**all commands**",
+  execute(message) {
+    let commands = message.client.commands.array();
+
+    let helpEmbed = new MessageEmbed()
+    .setDescription(`
+[{Click here}](https://discord.com/api/oauth2/authorize?client_id=794288634659995659&permissions=3439696&scope=bot) **to invite the bot.**
+`)
+
+  
+   .setColor("RANDOM");
+   message.react("<a:53EC4B02339C41BF9A83F5597463DBEB:853287599720235029>")
+    return message.channel.send(helpEmbed).catch(console.error);
+
+  }
+};
