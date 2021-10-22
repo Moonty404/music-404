0dff685d247fa2d553da64eb5f24a9d4ded82c95
Monty404 <83779017+Moonty404@users.noreply.github.com>
2021-10-22 12:51:53 +0300

    Update invite.js

--- others/invite.js
+++ others/invite.js
@@ -16,12 +16,12 @@ module.exports = {
 
     let helpEmbed = new MessageEmbed()
     .setDescription(`
-[{Click here}](https://discord.com/api/oauth2/authorize?client_id=794288634659995659&permissions=3439696&scope=bot) **to invite the bot.**
+[Click here](https://discord.com/api/oauth2/authorize?client_id=794288634659995659&permissions=3439696&scope=bot) to invite Meow Music.
 `)
 
   
    .setColor("RANDOM");
-   message.react("<a:53EC4B02339C41BF9A83F5597463DBEB:853287599720235029>")
+   message.react("<:yesaeon:900950547539054643>")
     return message.channel.send(helpEmbed).catch(console.error);
 
   }
