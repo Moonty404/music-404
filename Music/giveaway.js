14aa70ba6efbb9a8cd7bfd55f13c60d0501dcd9b
Monty404 <83779017+Moonty404@users.noreply.github.com>
2021-10-23 08:20:48 +0300

    Update giveaway.js

--- Music/giveaway.js
+++ Music/giveaway.js
@@ -33,7 +33,7 @@ React with 🎉 to enter!
 Hosted by: ${message.author}`)
       .setTimestamp()
       .setFooter(`Ends at`)
-      .setColor(`RANDOM`);
+      .setColor(`#F3692F`);
     //delete the Command
     message.delete({timeout: 300})
     let m = await channel.send(Embed);
