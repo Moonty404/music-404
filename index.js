793415e332b7ca1dfc1fab44f41f795f714755dd
Monty404 <83779017+Moonty404@users.noreply.github.com>
2021-10-25 21:00:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ client.on(`ready`, () => {
       });
 
     
-    client.user.setActivity(`${PREFIX}help | Users ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)},`, { type: "LISTENING"});
+    client.user.setActivity(`Type: ${PREFIX}help | ${client.guilds.cache.size} Servers,Users ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)},`, { type: "LISTENING"});
    
   
       }, (5000));
